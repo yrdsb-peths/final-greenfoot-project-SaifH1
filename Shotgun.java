0726a3fd00d11cc2afb1b9d6df25688d6a3fe9dd
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2023-05-31 08:56:27 -0400

    Update Shotgun.java

--- Shotgun.java
+++ Shotgun.java
@@ -8,41 +8,42 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Shotgun extends Actor
 {
-    GreenfootImage[] shoot = new GreenfootImage[6];
-    
+    GreenfootImage[] gunImages = new GreenfootImage[6];
     SimpleTimer animationTimer = new SimpleTimer();
+    boolean isShooting = false;
+    
     public Shotgun()
     {
-        GreenfootImage image = getImage();
-        image.scale(image.getWidth() - 275, image.getHeight() - 260);
-        setImage(image);
-        
+        // Load the animation images
+        for(int i = 0; i < gunImages.length; i++)
+        {
+            gunImages[i] = new GreenfootImage("images/Shotgun/shotgun"+ i + ".png");
+            gunImages[i].scale(200, 200);
+        }
+        setImage(gunImages[0]);
         animationTimer.mark();
-    
     }
     
     int imageIndex = 0;
     public void animateShotgun()
     {
-        if(animationTimer.millisElapsed() < 100)
-        {
+        // Delay the shot
+        if(animationTimer.millisElapsed() < 100) {
             return;
         }
-        for(int i = 0; i < shoot.length; i++)
-        {
-            shoot[i] = new GreenfootImage("images/Shotgun/shotgun"+ i + ".png");
-            shoot[i].scale(200, 200);
-        }
         animationTimer.mark();
-        setImage(shoot[imageIndex]);
-        imageIndex = (imageIndex + 1)  % shoot.length;
+        setImage(gunImages[imageIndex]);
+        imageIndex = (imageIndex + 1)  % gunImages.length;
     }
 
     public void act()
     {
-        // Add your action code here.
         if (Greenfoot.isKeyDown("space"))
         {
+            isShooting = true;
+        }
+        
+        if(isShooting){
             animateShotgun();
         }
     }

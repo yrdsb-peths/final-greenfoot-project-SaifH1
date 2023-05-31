eedf21c42a03479086acc1d0f13b5dadd4bc40c4
SaifH1 <130993395+SaifH1@users.noreply.github.com>
2023-05-31 09:00:45 -0400

    added bird flying animation

--- Bird.java
+++ Bird.java
@@ -8,6 +8,32 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Bird extends Actor
 {
+    GreenfootImage[] fly = new GreenfootImage[8];
+    SimpleTimer animationTimer = new SimpleTimer();
+    
+    public Bird()
+    {
+        for(int i = 0; i < fly.length; i++)
+        {
+            fly[i] = new GreenfootImage("images/elephant_idle/idle"+ i + ".png");
+            fly[i].scale(100, 100);
+        }
+        animationTimer.mark();
+        setImage(fly[0]);
+    }
+    
+    int imageIndex = 0;
+    public void animateBird()
+    {
+        if(animationTimer.millisElapsed() < 100)
+        {
+            return;
+        }
+        animationTimer.mark();
+        setImage(fly[imageIndex]);
+        imageIndex = (imageIndex + 1)  % fly.length;
+    }
+    
     int speed = 1;
     /**
      * Act - do whatever the Bird wants to do. This method is called whenever
@@ -16,5 +42,7 @@ public class Bird extends Actor
     public void act()
     {
         setLocation(getX()+speed, getY());
+        animateBird();
     }
 }
+
